9ee74ff03b6079214a27c348527a990e25eeb46e
MatheusPitsch <101012809+MatheusPitsch@users.noreply.github.com>
2022-07-02 14:43:46 -0300

    [change] Execution

---
+++ aula06/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula06
+{
+    internal class Cachorro
+    {
+        public string nameDog;
+        public string nameMaster;
+        public int ageDog;
+
+    }
+}
--- aula06/Program.cs
+++ aula06/Program.cs
@@ -10,6 +10,38 @@ namespace aula06
     {
         static void Main(string[] args)
         {
+            Cachorro[] cachorrada = new Cachorro[3];
+
+            for (int i = 0; i <  cachorrada.Length; i++)
+            {
+                Cachorro cao = new Cachorro();
+
+                Console.WriteLine($"Nome do cachorro:");
+                cao.nameDog = Console.ReadLine();
+
+                Console.WriteLine("\nNome do dono:");
+                cao.nameMaster = Console.ReadLine();
+
+                Console.WriteLine("\nIdade do cachorro:");
+                cao.ageDog = Convert.ToInt32(Console.ReadLine());
+
+                cachorrada[i] = cao;
+
+                Console.Clear();
+            }            
+
+            for (int i = 0; i < cachorrada.Length; i++)
+            {
+                if (cachorrada[i].ageDog >= 6)
+                {
+                    Console.WriteLine($"Nome: {cachorrada[i].nameDog},\n Idade: {cachorrada[i].ageDog},\n Dono: {cachorrada[i].nameMaster}");
+                }
+                Console.Clear();
+            }
+
+
+
+            Console.ReadLine();
         }
     }
 }
